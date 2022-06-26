bb5322d398d2594c01128a297b6d994b70954091
qwq-call <70079775+qwq-call@users.noreply.github.com>
2022-06-26 19:44:22 +0800

    Add files via upload

--- qq点赞.py
+++ qq点赞.py
@@ -1,6 +1,7 @@
 import pyautogui
 import time
 pyautogui.FAILSAFE =True
+pyautogui.PAUSE = 0.1
 
 mytime=0
 # mytime1=mytime
@@ -33,6 +34,7 @@ def mouseClick(clickTimes, lOrR, img, reTry):
                 pyautogui.click(location.x,location.y,clicks=clickTimes,interval=0.2,duration=0.2,button=lOrR)
                 print("单击左键",img)
                 print('点赞成功')
+                pyautogui.moveTo(location.x-60, location.y,duration=0.2)
                 mytime+=1
                 time_second =0
                 time_first=time.time()
@@ -40,26 +42,28 @@ def mouseClick(clickTimes, lOrR, img, reTry):
                 break
                     
             else:
-                scroll = -480
+                scroll = -350
                 pyautogui.scroll(int(scroll))
                 print("未找到匹配图片,0.1秒后重s试") 
-                # time.sleep(0.05)
+                time.sleep(0.05)
                 time_second= int(time.time()  -time_first )
                 print(time_second)
                 break
 
-img = "4.png"
-reTry = 1
+
 
 # mytime=1
 
 # while mytime>0:
-while 1==1:
-    if time_second<7:
-        mouseClick(1, "left", img, reTry)
-        time.sleep(0.02)
-    else:
-        break
-now=time.time()
-print('本次共点赞说说',mytime,"个,用时：",int(now-myvalue)-10,"秒")
-print('距离上次点赞过了：',(time_first-int(float(last_time)))/60/60,"小时")
+if __name__ == '__main__':
+    img = "4.png"
+    reTry = 1
+    while 1==1:
+        if time_second<7:#7秒后不点赞自动结束程序
+            mouseClick(1, "left", img, reTry)
+            # time.sleep(0.02)
+        else:
+            now=time.time()
+            print('本次共点赞说说',mytime,"个,用时：",int(now-myvalue)-10,"秒")
+            print('距离上次点赞过了：',(time_first-int(float(last_time)))/60/60,"小时")
+            break
